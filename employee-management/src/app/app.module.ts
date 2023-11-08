7f10e94e7f85e14eed418c55af2d86f9fa3f4ecc
SvenSven <mikorecare@gmail.com>
2023-11-08 12:17:17 +0800

    FEAT: Added table

--- employee-management/src/app/app.module.ts
+++ employee-management/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ModifyEmployeeComponent } from './components/modify-employee/modify-employee.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    ModifyEmployeeComponent
+    ModifyEmployeeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
