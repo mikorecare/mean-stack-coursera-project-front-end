a8011fdb29f13d012c25b0e28b86c164818327e7
SvenSven <mikorecare@gmail.com>
2023-11-10 02:14:18 +0800

    FEAT: Updated environments

--- employee-management/src/environments/environment.development.ts
+++ employee-management/src/environments/environment.development.ts
@@ -1,6 +1,12 @@
-export const environment = {
+// export const environment = {
+//     production: false,
+//     ASSET: "../../../assets/",
+//     API_URL: "http://localhost:3333/api/v1",
+//     WS_URL: "ws://localhost:3333",
+//   };
+
+  export const environment = {
     production: false,
     ASSET: "../../../assets/",
-    API_URL: "http://localhost:3333/api/v1",
-    WS_URL: "ws://localhost:3333",
+    API_URL: " https://recare-miko-server.onrender.com/api/v1",
   };
